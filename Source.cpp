13effc6a71e463b8463cc9594d54d753955f019a
mobec <mobec622@outlook.jp>
2017-05-22 15:17:00 +0900

    c++の内容を削除

--- Source.cpp
+++ Source.cpp
@@ -21,60 +21,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	// ＤＸライブラリ初期化処理
 	if (DxLib_Init() == -1) return -1;
 	
-	const int graph_start = 1401;
-	const int graph_end = 1687;
-	int num = 0;
-	std::string numname;
-	const std::string filename_num(" 9502-");
-	const std::string filetype(".bmp");
-	const std::string graph_directory("Rmai_graph/");
-	std::string filename;
-	std::vector<int> graph;
-
-	int soundBGM, soundVoice;
-	soundBGM = LoadSoundMem("077 44444-44444.wav");
-	soundVoice = LoadSoundMem("078 006-00005.wav");
-	for (int i = graph_start; i <= graph_end; i++) {
-		if (num < 10) {
-			numname = "00" + std::to_string(num);
-		}
-		else if (num < 100) {
-			numname = "0" + std::to_string(num);
-		}
-		else {
-			numname = std::to_string(num);
-		}
-		filename = graph_directory + std::to_string(i) + filename_num + numname + filetype;
-		char* file = new char[filename.size() + 1];
-		std::char_traits<char>::copy(file, filename.c_str(), filename.size() + 1);
-		graph.push_back(LoadGraph(file));
-		++num;
-	}
-	
-	int gr = LoadGraph("Rmai_graph/1401 9502-000.bmp");
-	std::vector<int>::iterator itr = graph.begin();
-	int  count = 0;
-	
 	// グラフィックの描画先を裏画面にセット
 	SetDrawScreen(DX_SCREEN_BACK);
-	
-	PlaySoundMem(soundBGM, DX_PLAYTYPE_LOOP, true);
-	PlaySoundMem(soundVoice, DX_PLAYTYPE_BACK, true);
 	
 	while (1)
 	{
 		// 画面を初期化(真っ黒にする)
 		ClearDrawScreen();
 
-		if (itr == graph.end())	itr = graph.begin();
-
-		DrawGraph(0, 0, *itr, false);
-
-		++count;
-		if (count > 1) {
-			++itr;
-			count = 0;
-		}
+		
 
 
 		// 裏画面の内容を表画面にコピーする
