31bbf396d1379cb39a2c695875235d1298d790e3
mobec <mobec622@outlook.jp>
2017-05-22 23:35:48 +0900

    画像のロードができるようになった

--- Source.cpp
+++ Source.cpp
@@ -21,22 +21,36 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 	// ＤＸライブラリ初期化処理
 	if (DxLib_Init() == -1) return -1;
-
+	//Loadfile-----------------------------------------
 	const char dir[] = "Rmai_graph/";
 	const char type[] = ".bmp";
-	char str[3];
+	char str[4];
 	int num = 0;
-	char str_num1[1];
-	char str_num2[2];
-	char filename[100];
+	int error_flag = 0;
+	char str_num1[2];
+	char str_num2[3];
+	char str_num[3];
+	char str_[100] = "\0";
+	char filename[100] = "\0";
 	struct Graph
 	{
 		int graph;
 		bool flag = false;
 	};
 	Graph gr[100];
+
+	//action
+	struct Action
+	{
+		int begin;
+		int end;
+	};
+	Action action[100];
+	int sum;
+
 	for (int i = 0; i < 100; i++) {
-		for (int j = 0; j < 100; j++) {
+		action[i].begin = num;
+		for (int j = 1; j < 100; j++) {
 			sprintf(str, "%d", i);
 			if (i < 10) {
 				str[2] = str[0];
@@ -48,45 +62,68 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 				str[1] = str[0];
 				str[0] = '0';
 			}
-
+			str[3] = '\0';
 
 			filename[0] = '\0';
-			if (j < 10) {
-				sprintf(str_num1, "%d", j);
-			}
-			else {
-				sprintf(str_num2, "%d", j);
-			}
+			sprintf(str_num, "%d", j);
+
+			strcat(filename, dir);
 			strcat(filename, str);
 			strcat(filename, " (");
-			strcat(filename, str_num1);
+			strcat(filename, str_num);
 			strcat(filename, ").bmp");
-			gr[i].graph = LoadGraph(filename);
-
-			gr[i].graph = LoadGraph(filename);
+			strcpy(str_, filename);
+			gr[num].graph = LoadGraph(filename);
+			if (gr[num].graph == -1) {
+				gr[num-1].flag = true;
+				error_flag++;
+				break;
+			}
+			else {
+				num++;
+				error_flag = 0;
+			}
 		}
-		
+		if (error_flag == 2) {
+			break;
+		}
+		action[i].end = num-1;
 	}
+	//----------------------------------------------
 
+	//FPS-------------------------------------------
+	const int FPS = 60;
+	int count;
+	int aa = LoadGraph("Rmai_graph/001 (1).bmp");
 	// グラフィックの描画先を裏画面にセット
 	SetDrawScreen(DX_SCREEN_BACK);
 	
-	
+	int acnum = 0;
+
 	while (1)
 	{
 		// 画面を初期化(真っ黒にする)
 		ClearDrawScreen();
-
+		//printfDx("%d %d\n", action[1].begin, action[1].end);
+		DrawGraph(0, 0, gr[acnum].graph, true);
+		DrawGraph(100, 100, aa, true);
+		++acnum;
+		if (acnum > action[0].end) {
+			acnum = action[0].begin;
+		}
+		
 
 		// 裏画面の内容を表画面にコピーする
 		ScreenFlip();
-		
+
 		// Windows 特有の面倒な処理をＤＸライブラリにやらせる
 		// -1 が返ってきたらループを抜ける
 		if (ProcessMessage() < 0) break;
 
 		// もしＥＳＣキーが押されていたらループから抜ける
 		if (CheckHitKey(KEY_INPUT_ESCAPE)) break;
+
+		//Sleep(1000 / 60);
 	}
 
 	// ＤＸライブラリ使用の終了処理
